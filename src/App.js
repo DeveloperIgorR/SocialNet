0c6e63eb51d761577045e2101d8f14c36cdab57b
DeveloperIgorR <bratca@inbox.ru>
2021-05-15 00:31:29 +0300

    Fix4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Messages from "./components/Messages/Messages";
+import Music from "./components/Music/Music";
 import Nav from "./components/Nav/Nav";
+import News from "./components/News/News";
 import Profiles from "./components/Profiles/Profiles";
+import Settings from "./components/Settings/Settings";
 const App = () => {
   return (
-    <div className="wrapper">
-      <Header />
-      <Nav />
-      <Profiles />
-    </div>
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <Nav />
+        <div className="wrapper-components">
+          <Route path="/Profiles" component={Profiles} />
+          <Route path="/Messages" component={Messages} />
+          <Route path="/News" component={News} />
+          <Route path="/Music" component={Music} />
+          <Route path="/Settings" component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,13 @@
 import s from "./Nav.module.css"
-const Nav = ()=>{
-    return(
-        <div className={s.nav}>
-        <div className={s.item}><a>Messages</a></div>
-        <div className={s.item}><a>Profiles</a></div>
-        <div className={s.item}><a>Contacts</a></div>
-        <div className={s.item}><a>Settings</a></div>
-      </div>
-    )
+const Nav = () => {
+  return (
+    <div className={s.nav}>
+      <div className={s.item}><a href="/Profiles">Profiles</a></div>
+      <div className={s.item}><a href="/Messages">Messages</a></div>
+      <div className={s.item}><a href="/News">News</a></div>
+      <div className={s.item}><a href="/Music">Music</a></div>
+      <div className={s.item}><a href="/Settings">Settings</a></div>
+    </div>
+  )
 };
 export default Nav;

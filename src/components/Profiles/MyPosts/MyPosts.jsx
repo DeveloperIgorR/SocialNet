484c58262c72c93cee3d76e84a80b51ce5c4b799
DeveloperIgorR <bratca@inbox.ru>
2021-05-22 01:50:12 +0300

    Fix11.1

--- src/components/Profiles/MyPosts/MyPosts.jsx
+++ src/components/Profiles/MyPosts/MyPosts.jsx
@@ -1,13 +1,19 @@
 import s from "./MyPosts.module.css"
 import Post from "./Posts/Post";
 const MyPosts = (props) => {
-    let PostsElements = props.PostsArray.map(p=><Post textmesage={p.text}/>)
+    let PostsElements = props.PostsArray.map(p=><Post textmesage={p.text}/>);
+    let newPostElement = React.createRef();
+    let addPosts = ()=>{
+       let text = newPostElement.current.value;
+       alert(text);
+    };  
+    
     return (
         <div> 
             <h3>My Posts</h3>
             <div>
-                <div><textarea></textarea></div>
-                <div><button>Add post</button></div>
+                <div><textarea ref={newPostElement}></textarea></div>
+                <div><button onClick={addPosts}>Add post</button></div>
             </div>
             <div className={s.items}>
              {PostsElements}

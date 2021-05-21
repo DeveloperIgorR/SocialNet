a79c6baf9f33a033cba9e6270aebc130e29cc7ff
DeveloperIgorR <bratca@inbox.ru>
2021-05-22 01:56:28 +0300

    Fix11.2

--- src/components/Profiles/MyPosts/MyPosts.jsx
+++ src/components/Profiles/MyPosts/MyPosts.jsx
@@ -1,13 +1,13 @@
+import React from 'react';
 import s from "./MyPosts.module.css"
 import Post from "./Posts/Post";
 const MyPosts = (props) => {
     let PostsElements = props.PostsArray.map(p=><Post textmesage={p.text}/>);
     let newPostElement = React.createRef();
     let addPosts = ()=>{
-       let text = newPostElement.current.value;
+        let text = newPostElement.current.value;
        alert(text);
     };  
-    
     return (
         <div> 
             <h3>My Posts</h3>
@@ -18,7 +18,6 @@ const MyPosts = (props) => {
             <div className={s.items}>
              {PostsElements}
             </div>
-            
         </div>)
 };
 

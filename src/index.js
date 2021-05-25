2d74773e7f23ffed3be330cac84ea657d94d9049
DeveloperIgorR <bratca@inbox.ru>
2021-05-25 23:45:47 +0300

    Fix16.1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { rerenderEntireTree } from './render';
-import state from './redux/state';
-
+import state, { addPost, updateNewPostText } from './redux/state';
+ let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App appState={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    )
+  }
 rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,4 @@
-import { rerenderEntireTree } from "../render"
+
 
 let state ={
     messagesPage: {

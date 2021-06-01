a0868923c08c41beeec864d46fe3900149c2acb3
DeveloperIgorR <bratca@inbox.ru>
2021-06-01 21:48:11 +0300

    Fix24

--- src/redux/profiles-reducer.js
+++ src/redux/profiles-reducer.js
@@ -1,18 +1,22 @@
 const ADD_POST = "ADD-POST"
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT"
-const profilesReducer = (state,action)=>{
-        if (action.type === ADD_POST) {
-        let newElement = {
-          id: 4,
-          text: state.newPostText,
-        }
-        state.PostsArray.push(newElement)
-        state.newPostText = ""
-      } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
+const profilesReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      let newElement = {
+        id: 4,
+        text: state.newPostText,
       }
-    return state
+      state.PostsArray.push(newElement)
+      state.newPostText = ""
+      return state
+    case UPDATE_NEW_POST_TEXT:
+      state.newPostText = action.newText;
+      return state
+    default:
+      return state
+  }
 }
 export default profilesReducer
-export const actionAddPost = () => ({type: ADD_POST}) 
+export const actionAddPost = () => ({ type: ADD_POST })
 export const actionUpdateNewPostText = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })

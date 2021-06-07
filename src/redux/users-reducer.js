f15a5085d58b07194c2b2370e9abb4e612a239fc
DeveloperIgorR <bratca@inbox.ru>
2021-06-07 23:09:34 +0300

    Fix31.1

---
+++ src/redux/users-reducer.js
@@ -0,0 +1,32 @@
+const FOLLOW = "FOLLOW"
+const UNFOLLOW = "UNFOLLOW"
+let initialState = {
+  users: [
+    { id: 1,followed:true, fullName: "Luk",information:"Hi, I am podavan",location:{country:Russia,city:Ivanovo} },
+    { id: 2,followed:false, fullName: "Petr",information:"Hi, I am jedi",location:{country:USA,city:NewYork} },
+    { id: 3,followed:false, fullName: "Vindu",information:"Hi, I am magistr",location:{country:India,city:Deli} },
+    { id: 4,followed:true, fullName: "Yoda",information:"Hi, I am master",location:{country:Germany,city:Berlin} },
+    
+  ],
+ }
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SEND_MESSAGE:
+      return {
+        ...state,
+        newMessageText : "",
+        MessagesArray: [...state.MessagesArray, { id: 7, message: state.newMessageText }],
+      }
+    case UPDATE_NEW_MESSAGE_TEXT:
+      return {
+         ...state,
+         newMessageText : action.newMessage
+         }
+     
+    default:
+      return state
+  }
+}
+export default usersReducer
+export const followAC = () => ({ type: FOLLOW })
+export const unfollowAC = (message) => ({ type: UNFOLLOW, newMessage: message })

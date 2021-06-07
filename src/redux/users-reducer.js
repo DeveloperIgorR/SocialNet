1d2ca680fa63789e7136e4504f65dda1896069ee
DeveloperIgorR <bratca@inbox.ru>
2021-06-07 23:57:39 +0300

    Fix31.3

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,5 +1,6 @@
 const FOLLOW = "FOLLOW"
 const UNFOLLOW = "UNFOLLOW"
+const SET_USERS = "SET_USERS"
 let initialState = {
   users: [
     { id: 1,followed:true, fullName: "Luk",information:"Hi, I am podavan",location:{country:Russia,city:Ivanovo} },
@@ -14,19 +15,34 @@ const usersReducer = (state = initialState, action) => {
     case FOLLOW:
       return {
         ...state,
-        users:[...state,users],
-        MessagesArray: [...state.MessagesArray, { id: 7, message: state.newMessageText }],
+        users:state.users.map(u=>{
+            if(u.id===action.userId){
+                return {...u.followed=true}
+            }
+            return u
+        }),
+        
       }
     case UNFOLLOW:
-      return {
-         ...state,
-         newMessageText : action.newMessage
+        return {
+            ...state,
+            users:state.users.map(u=>{
+                if(u.id===action.userId){
+                    return {...u.followed=false}
+                }
+                return u
+            }),
          }
-     
+
+    case  SET_USERS:
+        return{
+            ...state,users:[...state.users,...action.users]
+        }
     default:
       return state
   }
 }
 export default usersReducer
-export const followAC = () => ({ type: FOLLOW })
-export const unfollowAC = (message) => ({ type: UNFOLLOW, newMessage: message })
+export const followAC = (userId) => ({ type: FOLLOW,userId })
+export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
+export const setUsersAC = (users) => ({ type: SET_USERS, users })

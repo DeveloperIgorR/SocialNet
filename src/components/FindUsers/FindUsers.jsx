58d37a5fbaea42a5110a92cd580159669f3b0f80
DeveloperIgorR <bratca@inbox.ru>
2021-06-09 02:16:58 +0300

    Fix34

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import styles from './FindUsers.module.css'
-const FindUsers = (props)=>{
-    return<div>
-      {
-        props.users.map(u=><div key={u.id}>
-         <span>
-           <div>
+const FindUsers = (props) => {
+  // props.setUsers()
+  return <div>
+    {
+      props.users.map(u => <div key={u.id}>
+        <span>
+          <div>
             <img src={u.photoUrl} className={styles.styleUsersPhoto} />
-           </div>
-           <div>
-             <button>follow</button>
-           </div>
-         </span>
-         <span>
-           <div>{u.fullName}</div>
-           <div>{u.information}</div>
-           <div>{u.location.country}</div>
-         </span>
-        </div>)
-      }
-        
-          
-    </div>
-    
+          </div>
+          <div>
+            {u.followed
+              ? <button onClick={() => { props.unfollow(u.id) }}>Отписаться</button>
+              : <button onClick={() => { props.follow(u.id) }}>Подписаться</button>
+            }
+          </div>
+        </span>
+        <span>
+          <div>{u.fullName}</div>
+          <div>{u.information}</div>
+          <div>{u.location.country}</div>
+        </span>
+      </div>)
+    }
+
+
+  </div>
+
 }
 export default FindUsers
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,7 +3,7 @@ const UNFOLLOW = "UNFOLLOW"
 const SET_USERS = "SET_USERS"
 let initialState = {
   users: [
-    { id: 1,followed:true, photoUrl:"https://img4.goodfon.ru/original/1680x1050/0/df/stormtrooper-soldier-clone-sw-star-wars.jpg", fullName: "Luk",information:"Hi, I am podavan",location:{country:"Russia",city:"Ivanovo"} },
+    { id: 1,followed:false, photoUrl:"https://img4.goodfon.ru/original/1680x1050/0/df/stormtrooper-soldier-clone-sw-star-wars.jpg", fullName: "Luk",information:"Hi, I am podavan",location:{country:"Russia",city:"Ivanovo"} },
     { id: 2,followed:false, photoUrl:"https://avatars.mds.yandex.net/get-zen_doc/97540/pub_5d9267c99c944600ae6bfad0_5d9659f7a3f6e400ad99ddc4/scale_1200", fullName: "Petr",information:"Hi, I am jedi",location:{country:"USA",city:"NewYork"} },
     { id: 3,followed:false, photoUrl:"https://i.pinimg.com/736x/bd/7e/ba/bd7eba428adf0c4c3293269696ccd6b3.jpg",  fullName: "Vindu",information:"Hi, I am magistr",location:{country:"India",city:"Deli"} },
     { id: 4,followed:true, photoUrl:"https://avatars.mds.yandex.net/get-zen_doc/3721416/pub_603889bed4391d5d92ff062d_60389303d4391d5d920c7536/scale_1200", fullName: "Yoda",information:"Hi, I am master",location:{country:"Germany",city:"Berlin"} },
@@ -17,7 +17,7 @@ const usersReducer = (state = initialState, action) => {
         ...state,
         users:state.users.map(u=>{
             if(u.id===action.userId){
-                return {...u.followed=true}
+                return {...u,followed:true}
             }
             return u
         }),
@@ -28,7 +28,7 @@ const usersReducer = (state = initialState, action) => {
             ...state,
             users:state.users.map(u=>{
                 if(u.id===action.userId){
-                    return {...u.followed=false}
+                    return {...u,followed:false}
                 }
                 return u
             }),

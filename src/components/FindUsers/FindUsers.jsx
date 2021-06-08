6f827eb945b9483818649c959de691acf4688773
DeveloperIgorR <bratca@inbox.ru>
2021-06-08 23:00:48 +0300

    Fix33.1

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -1,17 +1,21 @@
+import React from 'react'
+import styles from './FindUsers.module.css'
 const FindUsers = (props)=>{
     return<div>
       {
         props.users.map(u=><div key={u.id}>
          <span>
            <div>
-            <img src="" />
+            <img src={u.photoUrl} className={styles.styleUsersPhoto} />
            </div>
            <div>
              <button>follow</button>
            </div>
          </span>
          <span>
-           <div></div>
+           <div>{u.fullName}</div>
+           <div>{u.information}</div>
+           <div>{u.location.country}</div>
          </span>
         </div>)
       }

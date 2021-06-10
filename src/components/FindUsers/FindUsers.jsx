6ed2faf439e6f0f0f31dd38f88b023075fa54281
DeveloperIgorR <bratca@inbox.ru>
2021-06-11 00:34:18 +0300

    Fix36

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -2,38 +2,40 @@ import * as axios from 'axios'
 import React from 'react'
 import styles from './FindUsers.module.css'
 import userImg from '../../assets/images/weider.png'
-const FindUsers = (props) => {
-  let getUsers = () => {
-  if(props.users.length===0){
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(respons=>{
-      props.setUsers(respons.data.items)
-    })}
-  }
-  return <div>
-    <button onClick={getUsers}>Get Users</button>
-    {
-      props.users.map(u => <div key={u.id}>
-        <span>
-          <div>
-            <img src={u.photos.small !=null ?u.photos.small:userImg} className={styles.styleUsersPhoto} />
-          </div>
-          <div>
-            {u.followed
-              ? <button onClick={() => { props.unfollow(u.id) }}>Отписаться</button>
-              : <button onClick={() => { props.follow(u.id) }}>Подписаться</button>
-            }
-          </div>
-        </span>
-        <span>
-          <div>{u.name}</div>
-          <div>{u.status}</div>
-          {/* <div>{u.location.country}</div> */}
-        </span>
-      </div>)
+class FindUsers extends React.Component {
+      getUsers = () => {
+      if (this.props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(respons => {
+          this.props.setUsers(respons.data.items)
+        })
+      }
     }
+    render() {
+    return <div>
+      <button onClick={this.getUsers}>Get Users</button>
+      {
+        this.props.users.map(u => <div key={u.id}>
+          <span>
+            <div>
+              <img src={u.photos.small != null ? u.photos.small : userImg} className={styles.styleUsersPhoto} />
+            </div>
+            <div>
+              {u.followed
+                ? <button onClick={() => { this.props.unfollow(u.id) }}>Отписаться</button>
+                : <button onClick={() => { this.props.follow(u.id) }}>Подписаться</button>
+              }
+            </div>
+          </span>
+          <span>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
+            {/* <div>{u.location.country}</div> */}
+          </span>
+        </div>)
+      }
 
 
-  </div>
-
+    </div>
+  }
 }
 export default FindUsers

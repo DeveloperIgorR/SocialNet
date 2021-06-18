6292f33d0a8edc8dc163f0fb4de0abbd43af7992
DeveloperIgorR <bratca@inbox.ru>
2021-06-19 00:36:40 +0300

    Fix46.1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import s from "./Header.module.css"
+import { NavLink } from "react-router-dom"
 const Header = () => {
     return (
         <div className={s.header}>
             <img src="https://64.media.tumblr.com/836a67a785b183482cb898f4f838ae1b/tumblr_nt55z4ylQF1t7b5qro1_1280.jpg" />
+            <div className={s.login}>
+               <NavLink to={'/login'}>Login</NavLink>
+            </div>
         </div>
     )
-};
-export default Header;
+}
+export default Header
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from "redux"
+import authReducer from "./auth-reducer"
 import messagesReducer from "./messages-reducer"
 import profilesReducer from "./profiles-reducer"
 import usersReducer from "./users-reducer"
 let reducers = combineReducers({
     profilesPage: profilesReducer,
     messagesPage: messagesReducer,
-    findUsersPage:usersReducer
+    findUsersPage:usersReducer,
+    auth:authReducer,
 })
 let store = createStore(reducers)
 

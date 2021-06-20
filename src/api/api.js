ae0ad22dcf17766607e1392893d5ec2d1cea208f
DeveloperIgorR <bratca@inbox.ru>
2021-06-20 21:54:26 +0300

    Fix48.2

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,14 @@
 import * as axios from 'axios'
-const instance = axios.create()
-export const getUsers = instance.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        {withCredentials:true})
+const instance = axios.create({
+    withCredentials: true,
+    headers: { 'API-KEY': 'b034670b-e3d4-4464-a5dc-9cfc4caad1a1' },
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/'
+})
+export const usersApi = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(respons => {
-                respons.data
+                return respons.data
             })
+    }
+}

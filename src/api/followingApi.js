e06dd2581e5b199f5d37af9e7bd26e0c03c5b042
DeveloperIgorR <bratca@inbox.ru>
2021-06-21 02:06:27 +0300

    Fix49

---
+++ src/api/followingApi.js
@@ -0,0 +1,23 @@
+import * as axios from 'axios'
+const instance = axios.create({
+    withCredentials: true,
+    headers: { 'API-KEY': 'b034670b-e3d4-4464-a5dc-9cfc4caad1a1' },
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/'
+})
+export const followingApi = {
+    setFollow(id) {
+        return instance.post(`follow/${id}`,{},)
+        .then(respons => {
+           if(respons.data.resultCode == 0)
+           {follow(id)}
+        })
+    },
+    setUnfollow(id){
+        return  instance.delete(`follow/${id}`)
+        .then(respons => {
+          if(respons.data.resultCode == 0)
+           {unfollow(id)}
+        })
+    }
+
+}
--- src/components/FindUsers/FindUsersContainer.jsx
+++ src/components/FindUsers/FindUsersContainer.jsx
@@ -1,6 +1,7 @@
 import * as axios from 'axios'
 import React from 'react'
 import { connect } from "react-redux"
+import { usersApi } from '../../api/getUsersApi'
 import { follow, setCurrentPage, setPreloader, setTotalCount, setUsers, unfollow } from "../../redux/users-reducer"
 import Preloader from '../Common/Preloader/Preloader'
 import FindUsers from './FindUsers'
@@ -8,27 +9,25 @@ import FindUsers from './FindUsers'
 class FindUsersAPI extends React.Component {
     componentDidMount() {
         this.props.setPreloader(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-        {withCredentials:true})
-            .then(respons => {
+        usersApi.getUsers(this.props.currentPage, this.props.pageSize)
+            .then(data => {
                 this.props.setPreloader(false)
-                this.props.setUsers(respons.data.items)
-                this.props.setTotalCount(respons.data.totalCount)
+                this.props.setUsers(data.items)
+                this.props.setTotalCount(data.totalCount)
             })
     }
     onPageChanged = (pageNumber) => {
         this.props.setPreloader(true)
         this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-        {withCredentials:true})
-            .then(respons => {
+        usersApi.getUsers(pageNumber,this.props.pageSize)
+            .then(data => {
                 this.props.setPreloader(false)
-                this.props.setUsers(respons.data.items)
+                this.props.setUsers(data.items)
             })
     }
     render() {
         return <>
-            {this.props.isFetching ? <Preloader/> : null}
+            {this.props.isFetching ? <Preloader /> : null}
             <FindUsers totalCount={this.props.totalCount}
                 pageSize={this.props.pageSize} currentPage={this.props.currentPage}
                 users={this.props.users} unfollow={this.props.unfollow}
@@ -45,6 +44,8 @@ let mapStateToProps = (state) => {
         isFetching: state.findUsersPage.isFetching,
     }
 }
-const FindUsersContainer = connect(mapStateToProps,{follow,unfollow,setUsers,setCurrentPage,
-    setTotalCount,setPreloader} )(FindUsersAPI)
+const FindUsersContainer = connect(mapStateToProps, {
+    follow, unfollow, setUsers, setCurrentPage,
+    setTotalCount, setPreloader
+})(FindUsersAPI)
 export default FindUsersContainer

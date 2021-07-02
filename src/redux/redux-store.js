d5e86ee848fa5eca1cd343296e4007e1bf91e651
DeveloperIgorR <bratca@inbox.ru>
2021-07-03 00:33:43 +0300

    Fix63

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,11 +4,13 @@ import messagesReducer from "./messages-reducer"
 import profilesReducer from "./profiles-reducer"
 import usersReducer from "./users-reducer"
 import thunkMiddleware from "redux-thunk"
+import {reducer as formReducer} from "redux-form"
 let reducers = combineReducers({
     profilesPage: profilesReducer,
     messagesPage: messagesReducer,
     findUsersPage:usersReducer,
     auth:authReducer,
+    form:formReducer,
 })
 let store = createStore(reducers,applyMiddleware(thunkMiddleware))
 
